14d2d4ac7910a22b523989a5884c20ce7dce789d
stovet <steven.topalian@gmail.com>
2021-12-30 11:19:54 -0500

    shop routes updated

--- src/routes/shop-routes.ts
+++ src/routes/shop-routes.ts
@@ -9,16 +9,50 @@ const shops: Shop[] = [
     { id: 444, name: "Sylvester's Shoes", rating: 3.8 },
     { id: 555, name: "Teddy's Tunes", rating: 4.7 }
 ];
+let nextId: number = 666;
 
 //Query ? minRating= 4.0
 shopRoutes.get("/", function(req, res){
-    //req.query.minRatin
-    let minRating: number = parseFloat(req.query.minRating as string);
+    let minRatingParam: string = req.query.minRating as string;
+    if(minRatingParam){
+         //req.query.minRatin
+        let minRating: number = parseFloat(minRatingParam);
 
-    // if shops[i].rating >= req.query.minRating
-    let filteredShops: Shop[] = shops.filter(shop => shop.rating >= minRating);
-    res.json(filteredShops);
+        // if shops[i].rating >= req.query.minRating
+        let filteredShops: Shop[] = shops.filter(shop => shop.rating >= minRating);
+        res.json(filteredShops);
+    } else {
+        res.json(shops);
+    }
+   
 });
+
+shopRoutes.post("/", function(req, res){
+    let newShop: Shop = req.body;
+    newShop.id = nextId;
+    nextId += 111;
+    shops.push(newShop);
+    res.status(201);
+    res.json(newShop);
+});
+
+shopRoutes.delete("/:id", function(req, res){
+    let inputId: number = parseInt(req.params.id);
+    // for(let i = 0; i < shops.length; i++){
+    //     if(shops[i].id === inputId){
+    //         shops.splice(i, 1)
+    //         break;
+    //     }
+    // }
+    
+    let shopIndex = shops.findIndex(shop => shop.id === inputId);
+    console.log(shopIndex);
+    shops.splice(shopIndex, 1);
+    res.status(204);
+    res.json("");
+});
+
+
 
 shopRoutes.get("/:id", function(req, res){
    // req.params.id shops[i].id
